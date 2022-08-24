ad476e111a2fcb64d2e50dd89862705b9997533a
Sebastian Roll <sebastianroll84@gmail.com>
2022-08-24 18:52:53 +0200

    Add draw method

--- python_properly/cardgame/deck.py
+++ python_properly/cardgame/deck.py
@@ -6,8 +6,19 @@ import itertools
 # learn: relative imports
 from .card import Card
 
+
+class DeckEmptyError(Exception):
+    ...
+
+
 class Deck:
     def __init__(self, shuffle=False):
         self.cards = list(Card.from_tuple(t) for t in itertools.product(range(2,15),"SHDC"))
         if shuffle:
             random.shuffle(self.cards)
+
+    def draw(self, numcards=1):
+        if numcards > len(self.cards):
+            raise DeckEmptyError()
+        drawed, self.cards = self.cards[:numcards], self.cards[numcards:]
+        return drawed
