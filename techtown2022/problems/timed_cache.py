3a3a13fbc7ba17d697296c86a553de06dd974a02
Peder Bergebakken Sundt <pbsds@hotmail.com>
2022-08-30 11:04:33 +0200

    add timed cache example

--- techtown2022/problems/timed_cache.py
+++ techtown2022/problems/timed_cache.py
@@ -11,3 +11,18 @@ import datetime
 # TASK: make a cache decorator that invalidates cached objects if the previous request is more than X seconds old
 def timed_cache(*, timeout: float):
     pass
+
+
+# You can run this using:
+#   $ poetry run python -m techtown2022.problems.timed_cache
+
+if __name__ == "__main__"_
+    @timed_cache(timeout = 10)
+    def my_function(foo, bar):
+        print(f"{foo = }")
+        print(f"{bar = }")
+        return foo + bar
+
+    print(f"{my_function(3, 5) = }")
+    print(f"{my_function(3, 5) = }")
+    print(f"{my_function(3, 5) = }")
